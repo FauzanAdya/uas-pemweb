2e3722c9fad3b46091123d6deba978b4a0088005
epunkk <evanchristianw@gmail.com>
2026-06-23 19:45:26 +0700

    memperbaiki status stok bahan kalau 0 statusnya habis dan sinkron database

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'database_buket');
+define('DB_NAME', 'buket_db');
 
 function getDB() {
     static $conn = null;
